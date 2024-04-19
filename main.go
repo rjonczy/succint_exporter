6306dddfb538740098ddfc6aa1e1aa013b2ad3b6
Robert Jonczy <robert.jonczy@gmail.com>
2024-04-19 13:29:20 +0200

    wip

--- main.go
+++ main.go
@@ -1,27 +1,72 @@
 package main
 
 import (
-	"log"
 	"net/http"
+	"os"
 
+	"github.com/alecthomas/kingpin/v2"
+	"github.com/go-kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/promlog"
+	"github.com/prometheus/common/promlog/flag"
+	"github.com/prometheus/common/version"
+	"github.com/prometheus/exporter-toolkit/web"
+	"github.com/prometheus/exporter-toolkit/web/kingpinflag"
 )
 
+var (
+	metricsPath = kingpin.Flag("metricspath", "Path under which to expose Prometheus metrics.").Default("/metrics").String()
+)
+
+func init() {
+	prometheus.MustRegister(version.NewCollector("succint_exporter"))
+}
+
 func main() {
-	registry := prometheus.NewRegistry()
+	promlogConfig := &promlog.Config{}
+	toolkitFlags := kingpinflag.AddFlags(kingpin.CommandLine, ":9103")
 
-	gauge := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "my_metric",
-		Help: "This is a simple gauge metric",
-	})
+	flag.AddFlags(kingpin.CommandLine, promlogConfig)
+	kingpin.Version(version.Print("succint_exporter"))
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
+	logger := promlog.New(promlogConfig)
 
-	registry.MustRegister(gauge)
+	level.Info(logger).Log("msg", "Starting succint_exporter", "version", version.Info())
+	level.Info(logger).Log("msg", "Build context", "context", version.BuildContext())
 
-	gauge.Set(1)
+	// c := newCollectdCollector(logger)
+	// prometheus.MustRegister(c)
 
-	http.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
+	// startCollectdServer(context.Background(), c, logger)
 
-	log.Println("Beginning to serve on port :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.Handle(*metricsPath, promhttp.Handler())
+	if *metricsPath != "/" {
+
+		landingConfig := web.LandingConfig{
+			Name:        "succint_exporter",
+			Description: "Prometheus Succint Exporter",
+			Version:     version.Info(),
+			Links: []web.LandingLinks{
+				{
+					Address: *metricsPath,
+					Text:    "Metrics",
+				},
+			},
+		}
+		landingPage, err := web.NewLandingPage(landingConfig)
+		if err != nil {
+			level.Error(logger).Log("err", err)
+			os.Exit(1)
+		}
+		http.Handle("/", landingPage)
+	}
+
+	srv := &http.Server{}
+	if err := web.ListenAndServe(srv, toolkitFlags, logger); err != nil {
+		level.Error(logger).Log("msg", "Error starting HTTP server", "err", err)
+		os.Exit(1)
+	}
+
 }
